6e2062dc3803e712dab7372b69e5926f9e95b981
RoniEdu <110207335+RoniEdu@users.noreply.github.com>
2022-12-07 23:57:18 -0300

    ATVB2

--- site-reactNoticia/src/components/SectionBanner/index.jsx
+++ site-reactNoticia/src/components/SectionBanner/index.jsx
@@ -5,7 +5,7 @@ export function SectionBanner() {
     return (
         <div id="banner">
             <h1><b>DESTAQUES</b></h1>
-            <section id="banner">
+            <section id="banner1">
                 <img src={imgbanner} alt="banner-img"/>
                     <h3><u>Rainha Elizabeth II morre aos 96 anos na Escócia</u></h3>
             </section>
